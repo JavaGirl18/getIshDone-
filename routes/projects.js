3f3e6968d0512e39f62381ea5d92574b40ff5e3b
JavaGirl18 <valenciacooper89@gmail.com>
2018-06-25 21:37:58 -0400

    updates to routes

--- routes/projects.js
+++ routes/projects.js
@@ -1,57 +1,55 @@
 var express = require('express');
 
-const router = express.Router({mergeParams: true})
+const router = express.Router({ mergeParams: true })
 const User = require('../db/usersSchema')
 const Projects = require('../db/projectSchema')
 
-const {ProjectsModel} = require('../db/projectSchema.js')
+const { ProjectsModel } = require('../db/projectSchema.js')
 //get route
-router.get('/', function(req, res, next) {
-User.findById(req.params.userId).then((user)=>{
-res.send({
-    user: user,
-    Projects: user.projects
-})
-})
+router.get('/', function (req, res, next) {
+    User.findById(req.params.userId).then((user) => {
+        res.send({
+            user: user,
+            Projects: user.projects
+        })
+    })
 });
 
 //show route
 router.get('/:id', async (req, res) => {
-  
-  ProjectsModel.findById(req.params.id)
-  .then((projects)=>{
-   res.send(projects)
- }) 
+
+    ProjectsModel.findById(req.params.id)
+        .then((projects) => {
+            res.send(projects)
+        })
 })
 
-  //create route
-  router.post('/', (req,res)=>{
-    const userId = req.params.userId
+//create route
+router.post('/new', (req, res) => {
     const newProject = new ProjectsModel(req.body)
-   User.findById(userId).then((user)=>{
-user.
-        res.send(projects)
+    newProject.save().then((project) => {
+        res.send(project)
     })
 })
 
 
 
 //update route
-router.put('/:id', async (req,res)=>{
-  const userId = req.params.id
-  const updatedUser = req.body
-  const savedUser = await UsersModel.findByIdAndUpdate(userId, updatedUser)
-  res.send(savedUser)
+router.put('/:id', async (req, res) => {
+            const projectId = req.params.id
+            const updatedProject = req.body
+            const savedProject = await ProjectsModel.findByIdAndUpdate(projectId, updatedProject)
+            res.send(savedProject)
 
-})
+        })
 
 //delete route
-router.put('/:id', async (req,res)=>{
-  const userId = req.params.id
-  
-  UsersModel = await UsersModel.findByIdAndRemove(userId)
-  res.send({msg: 'user deleted'})
+router.put('/:id', async (req, res) => {
+            const userId = req.params.id
 
-})
+            UsersModel = await UsersModel.findByIdAndRemove(userId)
+            res.send({ msg: 'user deleted' })
+
+        })
 
 module.exports = router;
